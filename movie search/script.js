39d0c251b13f7e72020d42bd63c8ddff76019a97
Abdo Mahmoud <abdelrahman12.dev@gmail.com>
2026-09-21 18:06:26 +0300

    version 2

--- movie search/script.js
+++ movie search/script.js
@@ -9,8 +9,8 @@ if(inputValue === ''){
 const url = `https://www.omdbapi.com/?apikey=d5dfca2f&s=${inputValue}`;
 const response = await fetch(url)
 const results = await response.json();
-if(results.response){
-  console.log('fine')
+if(!results.ok){
+ console('error something gone wrong try again later')
 }
 
  let html = '';
